460b379eb14e7d3951ce39dbbbef58297da622b5
Oscar Marin <oscar@marin.cr>
2017-07-08 18:01:08 -0600

    improvements

---
+++ Code/eyca.web/Controllers/InvoiceController.cs
@@ -0,0 +1,25 @@
+﻿using eyca.core.Data;
+using eyca.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eyca.web.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        private ItemRepository _repo;
+
+        public InvoiceController()
+        {
+            _repo = new ItemRepository(new TableClient<Item>("items"));
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
